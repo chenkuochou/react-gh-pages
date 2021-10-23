f45252a8111ef94d2cd62124bacf0a37bffd42fe
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2021-10-23 15:44:11 +1030

    finish

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
-import './topbar.scss'
-import { Person, Mail } from '@material-ui/icons'
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className={'topbar ' + (menuOpen && 'active')}>
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -19,7 +19,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
@@ -27,5 +27,5 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
